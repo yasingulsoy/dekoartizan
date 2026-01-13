c6a5f56e32dfd937c5cdf6803af63948157b6139
Yasin Gülsoy <yasingulsoy02@gmail.com>
2026-01-13 17:32:26 +0300

    Update Brands component to adjust height of separator div from 1px to 32px for improved layout.

--- src/components/homepage/Brands/index.tsx
+++ src/components/homepage/Brands/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Brands = () => {
   return (
     <div className="bg-black">
-      <div className="max-w-frame mx-auto h-px"></div>
+      <div className="max-w-frame mx-auto h-32"></div>
     </div>
   );
 };
