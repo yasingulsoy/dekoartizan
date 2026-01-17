754c9b00635ac3187eea6ac57cd506d8f694eba7
Yasin Gülsoy <yasingulsoy02@gmail.com>
2026-01-17 11:25:56 +0300

    Add value and required props to InputField component for enhanced functionality and validation support.

--- admin/src/components/form/input/InputField.tsx
+++ admin/src/components/form/input/InputField.tsx
@@ -6,6 +6,7 @@ interface InputProps {
   name?: string;
   placeholder?: string;
   defaultValue?: string | number;
+  value?: string | number;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   className?: string;
   min?: string;
@@ -15,6 +16,7 @@ interface InputProps {
   success?: boolean;
   error?: boolean;
   hint?: string; // Optional hint text
+  required?: boolean;
 }
 
 const Input: FC<InputProps> = ({
@@ -23,6 +25,7 @@ const Input: FC<InputProps> = ({
   name,
   placeholder,
   defaultValue,
+  value,
   onChange,
   className = "",
   min,
@@ -32,6 +35,7 @@ const Input: FC<InputProps> = ({
   success = false,
   error = false,
   hint,
+  required,
 }) => {
   // Determine input styles based on state (disabled, success, error)
   let inputClasses = `h-11 w-full rounded-lg border appearance-none px-4 py-2.5 text-sm shadow-theme-xs placeholder:text-gray-400 focus:outline-hidden focus:ring-3 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 ${className}`;
@@ -55,11 +59,13 @@ const Input: FC<InputProps> = ({
         name={name}
         placeholder={placeholder}
         defaultValue={defaultValue}
+        value={value}
         onChange={onChange}
         min={min}
         max={max}
         step={step}
         disabled={disabled}
+        required={required}
         className={inputClasses}
       />
 
