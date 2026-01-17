bdb82695484bca20ba4e36c18d94e3a1baa37095
Yasin Gülsoy <yasingulsoy02@gmail.com>
2026-01-17 11:28:06 +0300

    Enhance Button component by adding type prop for button type specification and updating className comment for clarity.

--- admin/src/components/ui/table/index.tsx
+++ admin/src/components/ui/table/index.tsx
@@ -29,6 +29,7 @@ interface TableCellProps {
   children: ReactNode; // Cell content
   isHeader?: boolean; // If true, renders as <th>, otherwise <td>
   className?: string; // Optional className for styling
+  colSpan?: number; // Number of columns to span
 }
 
 // Table Component
@@ -56,9 +57,14 @@ const TableCell: React.FC<TableCellProps> = ({
   children,
   isHeader = false,
   className,
+  colSpan,
 }) => {
   const CellTag = isHeader ? "th" : "td";
-  return <CellTag className={` ${className}`}>{children}</CellTag>;
+  return (
+    <CellTag className={` ${className}`} colSpan={colSpan}>
+      {children}
+    </CellTag>
+  );
 };
 
 export { Table, TableHeader, TableBody, TableRow, TableCell };
